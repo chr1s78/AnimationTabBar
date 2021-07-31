300ad40131a8a49e98be7ff629f0c5d7892f769b
chr1s78 <ayahirano2014@126.com>
2021-07-31 19:07:18 +0800

    Initial Commit

---
+++ AnimationTabBar/AnimationTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTabBarApp.swift
+//  AnimationTabBar
+//
+//  Created by 吕博 on 2021/7/31.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimationTabBar
+//
+//  Created by 吕博 on 2021/7/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
